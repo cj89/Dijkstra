ddb33ee479cfb731d28fe0d9093b4b23f8241189
cj89 <egyed.daniel89@gmail.com>
2016-04-26 19:47:54 +0200

    SecondCommit

---
+++ Dijkstra/Dijkstra/Classes/InputFileReader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Dijkstra.Classes
+{
+    /// <summary>
+    /// Input fájl beolvasása
+    /// </summary>
+    class InputFileReader
+    {
+        /// <summary>
+        ///Csúcsok száma
+        /// </summary>
+        public int N
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Start-csúcs
+        /// </summary>
+        public int S
+        {
+            get;
+            private set;
+        }
+
+        public InputFileReader()
+        {
+            InputCheck();
+
+
+        }
+
+        /// <summary>
+        /// Ellenőrizzük, hogy létezik-e az Input mappa,
+        /// illetve az input mappán belül a Graph.txt fájl.
+        /// </summary>
+        public void InputCheck()
+        {
+            string InputDirectoryPath = Directory.GetCurrentDirectory() + "\\Input";
+            string InputFile = InputDirectoryPath + "\\Graph.txt";
+
+            //Ha nem léteik a könyvtár
+            if (false == Directory.Exists(InputDirectoryPath))
+            {
+                //akkor elkészítjük
+                Directory.CreateDirectory(InputDirectoryPath);
+            }
+
+            FileInfo InputFileInfo = new FileInfo(InputFile);
+
+            //Ha nem létezik a fájl
+            if (false == InputFileInfo.Exists)
+            {
+                //akkor létrehozzuk
+                StreamWriter CreateStreamWriter = InputFileInfo.CreateText();
+                CreateStreamWriter.Close();
+            }
+        }
+    }
+}
---
+++ Dijkstra/Dijkstra/Program.cs
@@ -0,0 +1,14 @@
+﻿using Dijkstra.Classes;
+using System;
+
+namespace Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputFileReader a = new InputFileReader();
+            Console.ReadKey();
+        }
+    }
+}
